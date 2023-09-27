4a49773d357e070f1281421d197e3f07efe9713b
David Gil <64413374+davdgil@users.noreply.github.com>
2023-09-27 18:52:59 +0200

    ToDoList

---
+++ ToDoListJavaScript/scripts.js
@@ -0,0 +1,22 @@
+const add = () => {
+    const texto = document.getElementById('text1').value;
+
+    const tareaContainer = document.createElement('div');
+
+    var label = document.createElement('label');
+    label.textContent = texto;
+
+    var completarBtn = document.createElement('button');
+    completarBtn.textContent = 'Completar';
+
+    var eliminarBtn = document.createElement('button');
+    eliminarBtn.textContent = 'Eliminar';
+
+    tareaContainer.appendChild(label);
+    tareaContainer.appendChild(completarBtn);
+    tareaContainer.appendChild(eliminarBtn);
+
+    // Agrega la tarea al contenedorLabel
+    var contenedor = document.getElementById('labelContainer');
+    contenedor.appendChild(tareaContainer);
+}
